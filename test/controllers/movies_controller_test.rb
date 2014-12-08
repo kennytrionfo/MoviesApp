be3bf7c7fc1ed895a343287fdd4d825c21115f80
Kenny Trionfo <ktrionfo@integracore.com>
2014-12-08 15:49:40 -0700

    changed controller test file tests syntax to test from def

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -6,18 +6,18 @@ class MoviesControllerTest < ActionController::TestCase
     @movie ||= movies :kenny_is_king
   end
 
-  def test_index
+  test "index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:movies)
   end
 
-  def test_new
+  test "test_new" do
     get :new
     assert_response :success
   end
 
-  def test_create
+  test "create" do
     assert_difference('Movie.count') do
       post :create, movie: { date: movie.date, place: movie.place, title: movie.title }
     end
@@ -25,22 +25,22 @@ class MoviesControllerTest < ActionController::TestCase
     assert_redirected_to movie_path(assigns(:movie))
   end
 
-  def test_show
+  test "show" do
     get :show, id: movie
     assert_response :success
   end
 
-  def test_edit
+  test "edit" do
     get :edit, id: movie
     assert_response :success
   end
 
-  def test_update
+  test "update" do
     put :update, id: movie, movie: { date: movie.date, place: movie.place, title: movie.title }
     assert_redirected_to movie_path(assigns(:movie))
   end
 
-  def test_destroy
+  test "destroy" do
     assert_difference('Movie.count', -1) do
       delete :destroy, id: movie
     end
